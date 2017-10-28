48b6e6d5eeb1d31a57ef28491dcd9367a657a86c
Patrick Canny <patrickcanny@ku.edu>
2017-10-28 16:53:13 -0500

    Trying to figure out form validation on Python side

--- Landscrape/server/server.py
+++ Landscrape/server/server.py
@@ -1,8 +1,9 @@
 # server.py
 import random
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+app.debug = True
 
 @app.route("/")
 def index():
@@ -11,6 +12,11 @@ def index():
 @app.route("/search")
 def search():
     return render_template("Search.html")
+
+@app.route('/handle_data', methods = ['POST'])
+def handle_data():
+    print(request.form['Search.html'])
+
 
 if __name__ == "__main__":
     app.run()
--- Landscrape/static/js/Search.js
+++ Landscrape/static/js/Search.js
@@ -1,8 +1,3 @@
-$(".add").click(function(){
-  $("form>p:first-child").clone(true).inserBefore("form > p:last-child");
-  return false;
-});
-
-$(".remove").click(function(){
-  $(this).parent().remove();
-});
+function sendQueryToPython() {
+  
+}
