329b0ffbf3e8aa38fb01dd90a0ab5c0050dc9233
Patrick Canny <patrickcanny@ku.edu>
2017-11-24 17:45:33 -0700

    Cleaned up some stuff and added CueText for input

--- Landscrape/server/server.py
+++ Landscrape/server/server.py
@@ -24,20 +24,11 @@ app.debug = True
 ########################################################
 ## @Class InputForm
 #
-# Form that Takes the String(s) Given as Input
+# Form that Takes the String(s) Given as Input and passes them into the dictionary
 class InputForm(Form):
     search_query = StringField(u'Enter Your Search:', render_kw={"placeholder": "Please Enter Queries as a Comma-Seperated list."}, validators = [validators.input_required()])
     search_state = SelectField(u'State:', choices=[('AL','Alabama'), ('AK', 'Alaska'), ('AZ', 'Arizona'), ('AR', 'Arkansas'),('CA', 'California'), ('CO', 'Colorado'), ('CT', 'Connecticut'), ('DE', 'Delaware'),('FL', 'Florida'), ('GA', 'Georgia'), ('HI', 'Hawaii'), ('ID', 'Idaho'), ('IL', 'Illinois'), ('IN', 'Indiana'), ('IA', 'Iowa'), ('KS', 'Kansas'), ('KY', 'Kentucky'), ('LA', 'Louisiana'), ('ME', 'Maine'), ('MD', 'Maryland'), ('MA', 'Massachusetts'), ('MI', 'Michigan'), ('MN', 'Minnesota'), ('MS', 'Mississippi'), ('MO', 'Missouri'), ('MT', 'Montana'), ('NE', 'Nebraska'), ('NV', 'Nevada'), ('NH', 'New Hampshire'), ('NJ', 'New Jersey'), ('NM', 'New Mexico'), ('NY', 'New York'), ('NC', 'North Carolina'), ('ND', 'North Dakota'), ('OH', 'Ohio'), ('OK', 'Oklahoma'), ('OR', 'Oregon'), ('PA', 'Pennsylvania'), ('RI', 'Rhode Island'), ('SC', 'South Carolina'), ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'), ('VT', 'Vermont'), ('VA', 'Virginia'), ('WA', 'Washington'), ('WV', 'West Virginia'), ('WI', 'Wisconsin'), ('WY', 'Wyoming')], default='KS')
     search_city = StringField(u'City:', validators = [validators.input_required()], default="Lawrence")
-
-# class DynamicForm(Form):
-#     @classmethod
-#     def append_field(cls, name, field):
-#         setattr(cls, name, field)
-#         return cls
-
-
-
 
 
 ########################################################
