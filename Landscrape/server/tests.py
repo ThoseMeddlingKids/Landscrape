d24e58a09165149826a461410c8ee5ff320c9362
Patrick Canny <patrickcanny@ku.edu>
2017-11-24 19:53:18 -0700

    Unit testing Fix

--- Landscrape/server/tests.py
+++ Landscrape/server/tests.py
@@ -7,21 +7,20 @@ class TestScraper(unittest.TestCase):
     def test_Get_Output_Given_Input_Single(self):
         # Single Term Test, Checking if a single input produces something.
         testInput_SingleTerm= 'Coffee'
-        TestScraper = Scraper(testInput_SingleTerm)
-        self.assertEqual(TestScraper.get_results(), not None)
+        TestScraper = scrape.Scraper([testInput_SingleTerm,'Lawrence', 'Kansas'])
+        self.assertTrue(TestScraper.get_results())
 
     #This Tests that a dictionary is returned when the function "get_results" is called with multiple terms
     def test_Get_Output_Given_Input_Multi(self):
         # Multi-Term Test, Checking that something is returned.
         testInput_MultiTerm = 'Coffee, Bars, Tacos'
-        TestScraper = Scraper(testInput_MultiTerm)
-        self.assertEqual(TestScraper.get_results(), not None)
-        self.assertFalse(TestScraper.get_results(), None)
+        TestScraper = scrape.Scraper([testInput_MultiTerm, 'Lawrence', 'Kansas'])
+        self.assertTrue(TestScraper.get_results())
 
     # This Tests that the length of the output Dictionary is the same as the number of inputs
     def test_Get_Output_Given_Input_SameNumberOfDictEntriesAsInputs(self):
         testInput_MultiTerm = 'Coffee, Bars, Tacos'
-        TestScraper = Scraper(testInput_MultiTerm)
+        TestScraper = scrape.Scraper([testInput_MultiTerm,'Lawrence', 'Kansas'])
         self.assertEqual(len(TestScraper.get_results()), 3)
 
     #I think this is all the tests we need? Please review
